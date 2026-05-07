8266b1e103da5978b9e3ad7e9929453b8df73729
Juan Camilo Carreno Ramirez <jccarreno@unicauca.edu.co>
2026-05-06 23:38:25 -0500

    Inicializacion del proyecto

---
+++ src/test/java/com/accenture/prueba_tecnica/TestPruebaTecnicaApplication.java
@@ -0,0 +1,11 @@
+package com.accenture.prueba_tecnica;
+
+import org.springframework.boot.SpringApplication;
+
+public class TestPruebaTecnicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.from(PruebaTecnicaApplication::main).with(TestcontainersConfiguration.class).run(args);
+	}
+
+}
